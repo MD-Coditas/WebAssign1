233cebc6bf03aa484a0736fd3214e9b7250f2a64
MD-Coditas <mohit.desale@coditas.com>
2025-04-11 11:12:12 +0530

    Added Edit to Products

--- WebAssign1/Controllers/ProductController.cs
+++ WebAssign1/Controllers/ProductController.cs
@@ -33,8 +33,29 @@ namespace WebAssign1.Controllers
             return View();
         }
 
-        public IActionResult Edit()
+        public IActionResult Edit(int? id)
         {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            //Product? productFromDb = _db.Products.Find(id);
+            Product? productFromDb = _db.Products.FirstOrDefault(a=> a.Id == id);
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(productFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Products.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
             return View();
         }
 
